742651b9f200ae18f6888ac4ae69dda74c84a460
BrendomGoncalves <brendom.vi@gmail.com>
2024-02-07 02:19:21 -0300

    finalizando projeto de portifolio simples

---
+++ Semana_9/portifolio/src/app/app1/app1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-app1',
+  templateUrl: './app1.component.html',
+  styleUrl: './app1.component.css'
+})
+export class App1Component {
+
+}
