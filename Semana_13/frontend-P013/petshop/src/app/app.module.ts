464871c6af08949d58506be2a728739e1436882f
BrendomGoncalves <brendom.vi@gmail.com>
2024-03-06 15:55:56 -0300

    Removidos arquivos e estilos desnecessários

--- Semana_13/frontend-P013/petshop/src/app/app.module.ts
+++ Semana_13/frontend-P013/petshop/src/app/app.module.ts
@@ -1,36 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule, Routes } from '@angular/router';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CadastroAtendimentoComponent } from './cadastro-atendimento/cadastro-atendimento.component';
-import { ListagemAtendimentosComponent } from './listagem-atendimentos/listagem-atendimentos.component';
-// import { EdicaoAtendimentoComponent } from './edicao-atendimento/edicao-atendimento.component';
-// import { DetalheAtendimentoComponent } from './detalhe-atendimento/detalhe-atendimento.component';
-
-const routes: Routes = [
-  { path: 'cadastro-atendimento', component: CadastroAtendimentoComponent },
-  { path: 'listagem-atendimentos', component: ListagemAtendimentosComponent },
-  // { path: 'edicao-atendimento/:id', component: EdicaoAtendimentoComponent },
-  // { path: 'detalhe-atendimento/:id', component: DetalheAtendimentoComponent },
-  { path: '', redirectTo: '/listagem-atendimentos', pathMatch: 'full' },
-];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CadastroAtendimentoComponent,
-    ListagemAtendimentosComponent,
-    // EdicaoAtendimentoComponent,
-    // DetalheAtendimentoComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    RouterModule.forRoot(routes)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
